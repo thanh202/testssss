9aab25047fb997359585df000ab6a282ca924090
Thanh202 <lethanh27112002@gmail.com>
2023-10-23 13:43:45 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+      <Text>Nguyen Hau</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
